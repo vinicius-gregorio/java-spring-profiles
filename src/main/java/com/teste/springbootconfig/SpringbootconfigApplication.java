8b91f9097062898faa6a4fb5698d672c54818387
Vinicius Gregorio <vinicius.squadp@genial.com.vc>
2022-08-02 06:52:38 -0300

    initial commit

---
+++ src/main/java/com/teste/springbootconfig/SpringbootconfigApplication.java
@@ -0,0 +1,17 @@
+package com.teste.springbootconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootconfigApplication {
+
+	public static void main(String[] args) {
+		try {
+			SpringApplication.run(SpringbootconfigApplication.class, args);
+		} catch(Exception e){
+			throw new RuntimeException(e);
+		}
+	}
+
+}
